28679ee5ee3da441f60665e4bfa05407e9b77303
xmatzx <xmatzx@gmail.com>
2018-10-27 16:42:22 +0100

    update team model

--- app/models/team.js
+++ app/models/team.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const teamSchema = mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     isActive: {
         type: Boolean,
